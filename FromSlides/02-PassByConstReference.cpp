511876d2370610098db54f34984b2780cc31ea33
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2024-03-26 12:47:32 +0000

    More details

--- FromSlides/02-PassByConstReference.cpp
+++ FromSlides/02-PassByConstReference.cpp
@@ -1,8 +1,13 @@
 /* UP TO DATE */
 #include <iostream>
 
+// Takes a copy - may take variables or literals
 void fn(int a){};
+
+// Takes a reference - can only take variables
 void fn2(int & b){};
+
+// Takes a const reference - can take variables or literals and wont copy
 void fn3(const int & c){};
 
 int main(){
