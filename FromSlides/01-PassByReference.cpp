4f3ef8899e92dd5615d456851378b5c989402c4c
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2024-03-27 12:30:12 +0000

    Add value field
    Adds a value member to struct so we can show modification. Remember
    to add to copy constructor!

--- FromSlides/01-PassByReference.cpp
+++ FromSlides/01-PassByReference.cpp
@@ -4,35 +4,36 @@
 
 // This class is going to tell us if it is ever copied
 // It has no other purpose, except to pretend to be expensive
+// and to let us see if its value has been modified
 struct massive_data{
+  int value=0;
   // Constructor
   massive_data(){};
   // Copy constructor
-  massive_data(const massive_data & other){std::cout<<" ***** I'm being copied, this may take some time ***** \n";}
+  massive_data(const massive_data & other){std::cout<<" ***** I'm being copied, this may take some time ***** \n";value=other.value;}
 };
 
-int a_function(massive_data data){
-
+void a_function(massive_data data){
   std::cout<<"I'm a function who got some massive_data\n";
-  return 1;
+  ++data.value;
 };
-
 
 // You can think of this & as just meaning "pass this argument by reference"
-int a_function2(massive_data & data){
-
+void a_function2(massive_data & data){
   std::cout<<"I'm a function who got some massive_data by reference\n";
-  return 1;
+  ++data.value;
 };
 
 int main(){
 
   massive_data arg;
+  arg.value = 1; // Initial value
+
   std::cout<<"This is pass by value:\n";
   a_function(arg);
-
+  std::cout<<"Value after: "<<arg.value<<'\n';
 
   std::cout<<"This is pass by reference:\n";
   a_function2(arg);
-
+  std::cout<<"Value after: "<<arg.value<<std::endl;
 };
